bd1d09e1512616300a5371d3d5302b3c357180e9
aerocode <yichiu.ych@gmail.com>
2020-03-14 17:17:03 +0900

    add: test code

---
+++ src/test.ts
@@ -0,0 +1,20 @@
+import { uwait, wait, waitKey, waitLine, waitEnter, waitConfirm } from "./index";
+
+for(let i=0; i<10; i++){
+    console.log(i);
+
+    // console.log(uwait(333333));
+    // console.log(wait(333));
+    // console.log(waitKey());
+    // console.log(waitKey(27));
+    // console.log(waitKey(27, 333));
+    // console.log(waitKey(undefined, 333));
+    // console.log(waitEnter());
+    // console.log(waitEnter(333));
+    // console.log(waitLine("안녕하세요."));
+    // console.log(waitLine("안녕하세요.", 333));
+    // console.log(waitConfirm("네"));
+    // console.log(waitConfirm("네", "아니요"));
+    // console.log(waitConfirm("네", undefined, 1000));
+    // console.log(waitConfirm("네", "q", 1000));
+}
